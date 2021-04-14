f2b1c10c2dbd806757622a2ba7f718f58adae0e7
Joe Puzzo <jpuzzo@tesla.com>
2021-04-13 20:17:13 -0700

    maybe better solution

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,13 @@
-const fs = require('fs');
-const path = require('path');
-
-const modules = fs.readdirSync(path.join(__dirname, './src'))
-  .filter( file => file !== 'index.js')
-  .map( dir => {
-    const module = dir; // file.replace('.js', '');
-	  return {
-      input: `src/${module}/${module}.js`,
-      output: [
-      	// {
-        //   file: `dist/${module}.js`,
-        //   format: 'cjs'
-        // },
-        {
-          file: `dist/${module}.js`,
-          format: 'es'
-        }
-      ]
-    };
-  }
-);
-
-export default [
-  ...modules,
-];
+export default {
+  input: [`src/index.js`, 'src/a/a.js', 'src/b/b.js', 'src/c/c.js'],
+  output: [
+    {
+      dir: `dist/cjs`,
+      format: 'cjs'
+    },
+    {
+      dir: `dist/esm`,
+      format: 'esm'
+    }
+  ]
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { default as a } from './a.js';
-export { default as b } from './b.js';
-export { default as c } from './c.js';
+export { default as a } from './a/a.js';
+export { default as b } from './b/b.js';
+export { default as c } from './c/c.js';
